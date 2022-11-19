57617189c3259162feaeece09415f15520e62987
unknown <jakistamniewiemjtnw@gmail.com>
2022-11-19 18:36:23 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,6 @@ const Home: NextPage = () => {
       </Head>
       
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <ProductCard name="Cumpost" seller="Radziszop" price="12.50zl" weight="5kg" city="J-Bie" image="/coompost.png"/>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
