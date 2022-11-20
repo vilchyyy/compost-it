85859ff11935a3f5a32f86210260103cb696ceef
unknown <jakistamniewiemjtnw@gmail.com>
2022-11-20 11:06:45 +0100

    a

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -35,17 +35,23 @@ export default function Chat({ username }:props){
         priv.bind("private-chat-init", (data) => {
             if (data.init_by === username ){
                 privateChats[ data.receiver ] = pusher.subscribe( data.channelName );
-                console.log(privateChats[ data.receiver ])
-                privateChats[data.receiver].bind("chat-update", (data) => {
+                console.log(privateChats[ data.receiver ], data.receiver )
+                privateChats[data.receiver].bind("chat-update", (data: data) => {
                     const {message, username, time} = data
-                    console.log(data)
+                    setChats((prevstate: data[]) => [
+                        ...prevstate,
+                        {username, message, time}
+                    ])
                 })
             }else if(data.receiver === username) {
                 privateChats[ data.init_by ] = pusher.subscribe( data.channelName );
                 console.log(privateChats[ data.init_by ])
-                privateChats[data.init_by].bind("chat-update", (data) => {
+                privateChats[data.init_by].bind("chat-update", (data: data) => {
                     const {message, username, time} = data
-                    console.log(data)
+                    setChats((prevstate: data[]) => [
+                        ...prevstate,
+                        {username, message, time}
+                    ])
                 })
             }
         })
@@ -82,27 +88,34 @@ export default function Chat({ username }:props){
             sender: username,
             time
         })
-        setMessage("")
+        
     }
     const handleSubmit = async (e: any) => {
         e.preventDefault()
+        
         const date = new Date()
         const hour = date.getHours()>9?date.getHours().toString():"0"+date.getHours().toString()
         const minute = date.getMinutes()>9?date.getMinutes().toString():"0"+date.getMinutes().toString()
         const time = hour+":"+minute
-        if(Object.keys(privateChats).length > 0){
-            pusher.trigger(privateChats[receiver], "chat-update", {
-                message,
-                username,
-                time
+        
+            console.log(privateChats, receiver)
+            await pusher.trigger(privateChats[receiver].name, "chat-update", {
+                message: message,
+                username: username,
+                time: time
             })
-        }
+            setMessage("")
+        
         
     }
 
     return (
     <div className="bg-white relative border-2 border-gray-800 rounded-xl flex-col w-full">
-        <input type="text" onChange={(e)=>{setReceiver(e.target.value)}} />
+        <form onSubmit={handleSubmitForInit}>
+            <input type="text" onChange={(e)=>{setReceiver(e.target.value)}} />
+            <button type="submit">submit</button>
+        </form>
+        
         <div className="bg-green-500 w-full h-5 rounded-md flex items-center justify-end">
             <button onClick={() => setChatOpen(!chatOpen)} className="border-2 border-gray-900 rounded-md h-5 w-5 text-md tracking-tighter leading-none text-top pb-4 font-bold ">{chatOpen ? "X" : "^"}</button>
         </div>
