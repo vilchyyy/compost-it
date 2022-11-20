13813cc12a35da8016b69992b2c38331f805b9d3
radziszop <radek.krochmall@gmail.com>
2022-11-20 10:49:16 +0100

    fixed market styles

--- src/pages/listing/index.tsx
+++ src/pages/listing/index.tsx
@@ -1,20 +1,26 @@
-import {ProductCard} from "../../components/products/ProductCard"
+import { ProductCard } from "../../components/products/ProductCard";
 import Navbar from "../../components/Navbar";
 import { trpc } from "../../utils/trpc";
 
-export default function Index(){
-    const { data: listingsData } = trpc.listing.getSelfListings.useQuery();
-    return (
-        
+export default function Index() {
+  const { data: listingsData } = trpc.listing.getSelfListings.useQuery();
+  return (
     <div>
-        <Navbar/>
-        <div className="ml-64 p-6 flex flex-col">
-            {
-                listingsData?.map((listing: any) => (
-                    <ProductCard key={listing.id} id={listing.id} image={'/coompost.jpg'} price={listing.price} weight={listing.weight} city={listing.name} seller={listing.userId} name={listing.name} />
-                ))
-            }
-        </div>
+      <Navbar />
+      <div className="flex grow flex-wrap place-content-center">
+        {listingsData?.map((listing: any) => (
+          <ProductCard
+            key={listing.id}
+            id={listing.id}
+            image={"/coompost.jpg"}
+            price={listing.price}
+            weight={listing.weight}
+            city={listing.name}
+            seller={listing.userId}
+            name={listing.name}
+          />
+        ))}
+      </div>
     </div>
-    )
+  );
 }
